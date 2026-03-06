73cf6084c985f4779f4c40e068841ec992f0ab89
Mandar <smand@mandar>
2026-03-06 18:30:01 +0530

    SEO: Fixed sitemap index by removing placeholder links

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,9 +4,4 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/admin/*', '/api/*'],
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://fashionfabric.info/my-custom-sitemap-1.xml',
-    ],
-  },
 }
