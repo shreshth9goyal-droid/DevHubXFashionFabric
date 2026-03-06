4642e61ad10afe3f4c736ca234498c36ca9d4e33
Mandar <smand@mandar>
2026-03-06 17:41:31 +0530

    Standardized GTM placement and optimized SEO verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,18 +95,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${garamond.variable}`}>
       <head>
-        {/* Google Tag Manager (Script) - Placed as high in <head> as possible */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5DKSD76W');
-            `,
+              })(window,document,'script','dataLayer','GTM-5DKSD76W');`,
           }}
         />
         {/* DNS Prefetch for external resources */}
